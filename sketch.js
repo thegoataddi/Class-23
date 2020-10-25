97206af31911ea199ea1e6dd31f1f14e3553c1e9
thegoataddi <71863903+thegoataddi@users.noreply.github.com>
2020-10-25 13:49:36 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies= Matter.Bodies;
+var engine, world;
+var box1, box2, ground1;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,300,50,50);
+    box2 = new Box(200,100,50,50);
+    ground1 = new ground(200,380, 400, 20);
+}
+
+function draw(){
+    background("pink");
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    ground1.display();
+}
